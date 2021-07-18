2ae5fbe5fdaf11b44d4cdc74301e5e459b97be84
sento <sazaesanwa.kimo.kunai@gmail.com>
2021-07-18 12:26:56 +0900

    p37-38 - Dollar の equals を Money に移動

--- src/main/java/Dollar.java
+++ src/main/java/Dollar.java
@@ -6,9 +6,4 @@ class Dollar extends Money {
     Dollar times(int multiplier) {
         return new Dollar(this.amount * multiplier);
     }
-
-    public boolean equals(Object object) {
-        Dollar dollar = (Dollar) object;
-        return this.amount == dollar.amount;
-    }
 }
