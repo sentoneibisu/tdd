de6875b38308d7b0f44d1faab41dcc016992dba3
sento <sazaesanwa.kimo.kunai@gmail.com>
2021-07-18 12:04:15 +0900

    p31 - Dollar をコピーしただけの Franc クラスを作成 - Todo 1つ消化 - fix: テスト修正忘れ

--- src/main/java/MoneyTest.java
+++ src/main/java/MoneyTest.java
@@ -18,7 +18,7 @@ public class MoneyTest {
 
     @Test
     public void 乗算_Franc() {
-        Dollar five = new Franc(5);
+        Franc five = new Franc(5);
         assertEquals(new Franc(10), five.times(2));
         assertEquals(new Franc(15), five.times(3));
     }
