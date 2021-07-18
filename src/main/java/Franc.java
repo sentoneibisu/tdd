6105d7fc3ac45ba0d1e2b20c0896f43f5a7b664a
sento <sazaesanwa.kimo.kunai@gmail.com>
2021-07-18 12:36:46 +0900

    p39-41 - Fran の equals メソッドも消した

--- src/main/java/Franc.java
+++ src/main/java/Franc.java
@@ -1,16 +1,9 @@
-class Franc {
-    private int amount;
-
+class Franc extends Money {
     Franc(int amount) {
         this.amount = amount;
     }
 
     Franc times(int multiplier) {
         return new Franc(this.amount * multiplier);
-    }
-
-    public boolean equals(Object object) {
-        Franc franc = (Franc) object;
-        return this.amount == franc.amount;
     }
 }
